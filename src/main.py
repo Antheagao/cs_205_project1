734b5d37db6c64bebfecd5f3ec99badbbdac7803
Antheagao <Anthonymendez035@gmail.com>
2023-05-11 22:13:58 -0700

    first commit

---
+++ src/main.py
@@ -0,0 +1,20 @@
+""" This is the nine men in a trench puzzle project
+
+This project is an ai to solve the trench puzzle. 
+The trench puzzle is a puzzle where you have 9 people in a trench 
+with 13 spaces. The goal is to get all 9 people to their numbered 
+spaces in the trench. For example, the sergent is number 1 and 
+wishes to place himself at point 1.
+    
+"""
+
+__version__ = '0.1'
+__author__ = 'Anthony Mendez'
+
+
+def main():
+    num = None
+
+
+if __name__ == '__main__':
+    main()
